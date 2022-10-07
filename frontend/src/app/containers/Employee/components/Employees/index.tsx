d3bbe61ea90e9bf60cb4fe4347b145a8cbe7a127
DuyHV <duyhuynh.dn@gmail.com>
2022-10-07 11:13:47 +0700

    Add

--- frontend/src/app/containers/Employee/components/Employees/index.tsx
+++ frontend/src/app/containers/Employee/components/Employees/index.tsx
@@ -229,7 +229,7 @@ export class Employees extends React.Component<
                   className={`${style.icon} ${style['i-plus']} ${style['margin-right']}`}
                   aria-hidden='true'
                 />
-                Add New Employee 1123344
+                Add New Employee 555
               </Button>
             </div>
           </div>
